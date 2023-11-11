80cdc83430b6d03efddf6f7079bfde5ecc22e5ac
louis hugo <luihugo247@gmail.com>
2023-11-11 15:31:59 +0300

    adding warehousing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,38 @@
-import TodoItem from "@/components/TodoItem";
-import prisma from "@/db";
 import Link from "next/link";
 import React from "react";
-
-const getTOdos = () => {
-  return prisma.todo.findMany();
-};
-
-const toggleTodo = async (id: string, complete: boolean) => {
-  "use server";
-  await prisma.todo.update({ where: { id }, data: { complete } });
-};
-
-const Home = async () => {
-  const todos = await getTOdos();
-  // await prisma.todo.create({ data: { title: "test", complete: false } });
+const projects = [
+  { name: "TO-DO", link: "schedule" },
+  { name: "WAREHOUSE", link: "warehouse" },
+];
+const HomePage = () => {
   return (
-    <>
-      <header className="flex justify-between items-center mb-4 ">
-        <h1 className="text-2xl">TO-DOS</h1>
-        <Link
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href="/new"
-        >
-          New
-        </Link>
-      </header>
-      <ul>
-        {todos.map((todo, i) => {
-          return <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />;
+    <div className="container mx-auto py-2 px-2">
+      <h1 className="text-2xl text-slate-100 text-center underline mb-5 md:mb-10">
+        Examples
+      </h1>
+      <div className="flex flex-row  ">
+        {projects.map((project, i) => {
+          return (
+            <div
+              key={i}
+              className="flex relative border border-blue-700 w-1/2 md:w-1/3  bg-slate-800 hover:bg-slate-900 cursor-pointer h-40 w- items-center justify-center "
+            >
+              <Link className="py-15 px-15 md:px-30" href={`/${project.link}`}>
+                <text className="text-2xl text-center text-neutral-200 hover:text-neutral-100">
+                  {project.name}
+                </text>
+              </Link>
+              <Link href={`/${project.link}`}>
+                <div className="absolute bottom-0 right-0 pr-2 text-orange-300">
+                  <text>Go to &#8594;</text>
+                </div>
+              </Link>
+            </div>
+          );
         })}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 };
 
-export default Home;
+export default HomePage;
---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,38 @@
+import TodoItem from "@/components/TodoItem";
+import prisma from "@/db";
+import Link from "next/link";
+import React from "react";
+
+const getTOdos = () => {
+  return prisma.todo.findMany();
+};
+
+const toggleTodo = async (id: string, complete: boolean) => {
+  "use server";
+  await prisma.todo.update({ where: { id }, data: { complete } });
+};
+
+const Home = async () => {
+  const todos = await getTOdos();
+  // await prisma.todo.create({ data: { title: "test", complete: false } });
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4 ">
+        <h1 className="text-2xl">TO-DOS</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/schedule/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul>
+        {todos.map((todo, i) => {
+          return <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
